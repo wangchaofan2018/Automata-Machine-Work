35d1ae4da72cc9dc6eb46eba781eda47abad43b2
wangchaofan <wangchaofan@kkworld.com>
2020-10-29 15:54:46 +0800

    恢复.idea

--- src/NFA/NFA.java
+++ src/NFA/NFA.java
@@ -29,11 +29,9 @@ public class NFA {
     public static LinkedList<Status> toWhere(String from, char way, int index) {
         LinkedList<Status> temp = new LinkedList<>();
         LinkedList<Status> queue = new LinkedList<>();
-        List<Status> shouldRemove = new ArrayList<>();
         list.forEach(u->{
             if (u.from.equals(from) && way == u.way&&!"REFUSE".equals(u.to)) {
                 queue.add(new Status(u.from,u.way,u.to));
-                shouldRemove.add(u);
             }
         });
         while (!queue.isEmpty()) {
@@ -48,14 +46,10 @@ public class NFA {
         if (temp.isEmpty())return temp; //用于防止打印无效处理
         System.out.println("***当前处理第"+(index+1)+"个字符***");
         System.out.println(temp);
-        list.removeAll(shouldRemove);
         return temp;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-
+    public static void processed(String str){
         LinkedList<Status> toList = toWhere(BEGIN_STATUS,str.charAt(0),0);
         for (int i=1;i<str.length();i++) {
             LinkedList<Status> temp = new LinkedList<>();
@@ -64,17 +58,28 @@ public class NFA {
                 temp.addAll(toWhere(cur.to, str.charAt(i),i));
             }
             if (temp.isEmpty()){
-                System.out.println("字符串被拒绝");
+                System.out.println("***当前处理第"+(i+1)+"个字符");
+                System.out.println("自动机识别字符 '"+str.charAt(i)+"' 不合法,字符串被拒绝");
                 return;
             }
             toList.addAll(temp);
         }
         List<String> result = toList.stream().map(u -> u.to).collect(Collectors.toList());
         if (result.contains(END_STATUS)) {
-            System.out.println("执行完毕 处于自动机终止状态"+END_STATUS+" 字符串被接受");
+            System.out.println("***当前字符串处理完毕***");
+            System.out.println("处于自动机终止状态"+END_STATUS+" 字符串被接受");
         }else {
-            System.out.println("字符串被拒绝");
+            System.out.println("***当前字符串处理完毕***");
+            System.out.println("字符串未包含终止状态被拒绝");
         }
     }
-}
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = null;
+        while(!(str=sc.nextLine()).equals("exit")){
+            processed(str);
+        }
+        System.out.println("bye");
+    }
+}
